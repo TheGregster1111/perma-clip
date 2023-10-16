d3d995edafdbb0c691669b0e56092201f0a82cbd
Susanne <rasmusljung100@gmail.com>
2023-10-16 11:59:11 +0200

    Minor tweaks and indentation fix

--- perma-clip@susannedev.com/extension.js
+++ perma-clip@susannedev.com/extension.js
@@ -23,105 +23,108 @@ function init() {}
 
 function updateCheck ()
 {
-	let tempClip = settings.get_strv('clip');
-	let tempClipNames = settings.get_strv('clipnames');
-	
-	for (let i = 0; i < clip.length; i += 1)
-	{
-		if (tempClip[i] !== clip[i])
-		{
-			updateList();
-			return;
-		}
-	}
+    let tempClip = settings.get_strv('clip');
+    let tempClipNames = settings.get_strv('clipnames');
+    
+    for (let i = 0; i < clip.length; i += 1)
+    {
+        if (tempClip[i] !== clip[i])
+        {
+            updateList();
+            return;
+        }
+    }
 
-	for (let i = 0; i < clipNames.length; i += 1)
-	{
+    for (let i = 0; i < clipNames.length; i += 1)
+    {
 
-		if (tempClipNames[i] !== clipNames[i])
-		{
-			updateList();
-			return;
-		}
-	}
+        if (tempClipNames[i] !== clipNames[i])
+        {
+            updateList();
+            return;
+        }
+    }
 }
 
 function updateList()
 {
-	settings = extensionUtils.getSettings();
-	clip = settings.get_strv('clip');
-	clipNames = settings.get_strv('clipnames');
-	
-	menu.removeAll();
+    settings = extensionUtils.getSettings();
+    clip = settings.get_strv('clip');
+    clipNames = settings.get_strv('clipnames');
+    
+    menu.removeAll();
 
-	menu.addAction("Edit", () => {
-		extensionUtils.openPrefs();
-	});
+    menu.addAction("Edit", () => {
+        extensionUtils.openPrefs();
+    });
 
-	for (let i = 0; i < 10; i++) {
-		menu.addAction(clipNames[i], () => {
-			virtualKeyboard.notify_keyval(Clutter.get_current_event_time(), Clutter.KEY_Escape, Clutter.KeyState.PRESSED);
-			virtualKeyboard.notify_keyval(Clutter.get_current_event_time(), Clutter.KEY_Escape, Clutter.KeyState.RELEASED);
-			clipboard.set_text(St.ClipboardType.CLIPBOARD, clip[i]);
-			timeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 150, () => {
-				virtualKeyboard.notify_keyval(Clutter.get_current_event_time(), Clutter.KEY_Control_L, Clutter.KeyState.RELEASED);
-				virtualKeyboard.notify_keyval(Clutter.get_current_event_time(), Clutter.KEY_v, Clutter.KeyState.RELEASED);
+    for (let i = 0; i < 10; i++) {
+        menu.addAction(clipNames[i], () => {
+            virtualKeyboard.notify_keyval(Clutter.get_current_event_time(), Clutter.KEY_Escape, Clutter.KeyState.PRESSED);
+            virtualKeyboard.notify_keyval(Clutter.get_current_event_time(), Clutter.KEY_Escape, Clutter.KeyState.RELEASED);
+            clipboard.set_text(St.ClipboardType.CLIPBOARD, clip[i]);
+            timeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 150, () => {
+                virtualKeyboard.notify_keyval(Clutter.get_current_event_time(), Clutter.KEY_Control_L, Clutter.KeyState.RELEASED);
+                virtualKeyboard.notify_keyval(Clutter.get_current_event_time(), Clutter.KEY_v, Clutter.KeyState.RELEASED);
 
-				virtualKeyboard.notify_keyval(Clutter.get_current_event_time(), Clutter.KEY_Control_L, Clutter.KeyState.PRESSED);
-				virtualKeyboard.notify_keyval(Clutter.get_current_event_time(), Clutter.KEY_Shift_L, Clutter.KeyState.PRESSED);
-				virtualKeyboard.notify_keyval(Clutter.get_current_event_time(), Clutter.KEY_v, Clutter.KeyState.PRESSED);
-				virtualKeyboard.notify_keyval(Clutter.get_current_event_time(), Clutter.KEY_Control_L, Clutter.KeyState.RELEASED);
-				virtualKeyboard.notify_keyval(Clutter.get_current_event_time(), Clutter.KEY_Shift_L, Clutter.KeyState.RELEASED);
-				virtualKeyboard.notify_keyval(Clutter.get_current_event_time(), Clutter.KEY_v, Clutter.KeyState.RELEASED);
-
-				return GLib.SOURCE_REMOVE;
-			});
-		});
-	}
+                virtualKeyboard.notify_keyval(Clutter.get_current_event_time(), Clutter.KEY_Control_L, Clutter.KeyState.PRESSED);
+                virtualKeyboard.notify_keyval(Clutter.get_current_event_time(), Clutter.KEY_Shift_L, Clutter.KeyState.PRESSED);
+                virtualKeyboard.notify_keyval(Clutter.get_current_event_time(), Clutter.KEY_v, Clutter.KeyState.PRESSED);
+                virtualKeyboard.notify_keyval(Clutter.get_current_event_time(), Clutter.KEY_Control_L, Clutter.KeyState.RELEASED);
+                virtualKeyboard.notify_keyval(Clutter.get_current_event_time(), Clutter.KEY_Shift_L, Clutter.KeyState.RELEASED);
+                virtualKeyboard.notify_keyval(Clutter.get_current_event_time(), Clutter.KEY_v, Clutter.KeyState.RELEASED);
+                
+                timeoutId = null;
+                return GLib.SOURCE_REMOVE;
+            });
+        });
+    }
 }
 
 function enable()
 {
-	// Get user clipboard and create virtual keyboard
-	clipboard = St.Clipboard.get_default();
-	virtualKeyboard = Clutter.get_default_backend().get_default_seat().create_virtual_device(Clutter.InputDeviceType.KEYBOARD_DEVICE);
-	// Get user clipboard and create virtual keyboard
-	
+    // Get user clipboard and create virtual keyboard
+    clipboard = St.Clipboard.get_default();
+    virtualKeyboard = Clutter.get_default_backend().get_default_seat().create_virtual_device(Clutter.InputDeviceType.KEYBOARD_DEVICE);
+    // Get user clipboard and create virtual keyboard
+    
 
-	// Fix menu
-	panelIcon = new PanelMenu.Button(0.0, 'Perma Clip', false);
-	scrollView = new St.ScrollView();
-	menu = new PopupMenu.PopupMenuSection();
+    // Fix menu
+    panelIcon = new PanelMenu.Button(0.0, 'Perma Clip', false);
+    scrollView = new St.ScrollView();
+    menu = new PopupMenu.PopupMenuSection();
 
-	const icon = new St.Icon({
-		icon_name: 'emblem-documents',
-		style_class: 'system-status-icon'
-	});
-	panelIcon.add_child(icon);
-	scrollView.add_actor(menu.actor);
-	panelIcon.menu.box.add_child(scrollView);
-	
-	updateList();
+    const icon = new St.Icon({
+        icon_name: 'emblem-documents',
+        style_class: 'system-status-icon'
+    });
+    panelIcon.add_child(icon);
+    scrollView.add_actor(menu.actor);
+    panelIcon.menu.box.add_child(scrollView);
+    
+    updateList();
 
-	interval = setInterval(() => updateCheck(), 200);
-	// Fix menu
+    interval = setInterval(() => updateCheck(), 200);
+    // Fix menu
 
-	// Add the indicator to the panel
-	Main.panel.addToStatusArea('perma-clip@susannedev.com', panelIcon);
+    // Add the indicator to the panel
+    Main.panel.addToStatusArea('perma-clip@susannedev.com', panelIcon);
 }
 
 function disable()
 {
-	panelIcon.destroy();
+    panelIcon.destroy();
     panelIcon = null;
-	scrollView.destroy();
-	scrollView = null;
-	menu.destroy();
+    scrollView.destroy();
+    scrollView = null;
+    menu.destroy();
     menu = null;
     virtualKeyboard = null;
-	clipboard = null;
-	clearInterval(interval);
-	if (timeoutId) {
-		GLib.Source.remove(timeoutId);
-	}
+    clipboard = null;
+    settings = null;
+    clearInterval(interval);
+    
+    if (timeoutId) {
+        GLib.Source.remove(timeoutId);
+    }
 }
--- perma-clip@susannedev.com/prefs.js
+++ perma-clip@susannedev.com/prefs.js
@@ -7,12 +7,12 @@ let interval;
 
 function update (dropDown, input1Buffer, input2Buffer, grid, clip, clipNames, interval)
 {
-	if (dropDown.get_selected() != selected)
-	{
-		selected = dropDown.get_selected();
-		input1Buffer.set_text(clipNames[dropDown.get_selected()], clipNames[dropDown.get_selected()].length);
-		input2Buffer.set_text(clip[dropDown.get_selected()], clip[dropDown.get_selected()].length);
-	}
+    if (dropDown.get_selected() != selected)
+    {
+        selected = dropDown.get_selected();
+        input1Buffer.set_text(clipNames[dropDown.get_selected()], clipNames[dropDown.get_selected()].length);
+        input2Buffer.set_text(clip[dropDown.get_selected()], clip[dropDown.get_selected()].length);
+    }
 }
 
 function init () 
@@ -23,63 +23,63 @@ function init ()
 function fillPreferencesWindow(window)
 {
     let settings = extensionUtils.getSettings();
-	let clip = settings.get_strv('clip')
-	let clipNames = settings.get_strv('clipnames')
+    let clip = settings.get_strv('clip')
+    let clipNames = settings.get_strv('clipnames')
 
-	let page = new Adw.PreferencesPage();
-	let group = new Adw.PreferencesGroup();
+    let page = new Adw.PreferencesPage();
+    let group = new Adw.PreferencesGroup();
 
     let grid = new Gtk.Grid();
-	grid.set_row_homogeneous(true);
-	grid.set_column_homogeneous(true);
+    grid.set_row_homogeneous(true);
+    grid.set_column_homogeneous(true);
 
     let dropDown = Gtk.DropDown.new_from_strings(Array.from({ length: 10 }, (v, i) => clipNames[i] )); //Have to do it this way for some reason.
     grid.attach(dropDown, 4, 1, 2, 1);
 
-	let emptyLabel1 = new Gtk.Label();	//emptyLabelN items add space between menu items.
-	grid.attach(emptyLabel1, 0, 2, 1, 1);
+    let emptyLabel1 = new Gtk.Label();	//emptyLabelN items add space between menu items.
+    grid.attach(emptyLabel1, 0, 2, 1, 1);
 
     let input1 = new Gtk.Entry();
-	let input1Buffer = input1.get_buffer();
-	input1Buffer.set_max_length(10);
-	input1Buffer.set_text(clipNames[selected], clipNames[selected].length);
+    let input1Buffer = input1.get_buffer();
+    input1Buffer.set_max_length(10);
+    input1Buffer.set_text(clipNames[selected], clipNames[selected].length);
     grid.attach(input1, 1, 3, 8, 1);
 
-	let emptyLabel2 = new Gtk.Label();
-	grid.attach(emptyLabel2, 0, 4, 1, 1);
+    let emptyLabel2 = new Gtk.Label();
+    grid.attach(emptyLabel2, 0, 4, 1, 1);
 
-	let input2 = new Gtk.TextView();
-	let input2Buffer = input2.get_buffer();
-	input2.get_buffer().set_text(clip[selected], clip[selected].length);
+    let input2 = new Gtk.TextView();
+    let input2Buffer = input2.get_buffer();
+    input2.get_buffer().set_text(clip[selected], clip[selected].length);
     grid.attach(input2, 1, 5, 8, 3);
 
-	let emptyLabel3 = new Gtk.Label();
-	grid.attach(emptyLabel3, 0, 9, 1, 1);
+    let emptyLabel3 = new Gtk.Label();
+    grid.attach(emptyLabel3, 0, 9, 1, 1);
 
-	let button = new Gtk.Button();
-	button.set_label("Submit");
-	button.connect('clicked', 
-	() => { //Update dropdown menu to match new clipNames and update database.
-		clipNames[selected] = input1Buffer.get_text();
-		clip[selected] = input2Buffer.get_text(input2Buffer.get_start_iter(), input2Buffer.get_end_iter(), false);
-		settings.set_strv('clip', clip);
-		settings.set_strv('clipnames', clipNames);
+    let button = new Gtk.Button();
+    button.set_label("Submit");
+    button.connect('clicked', 
+    () => { //Update dropdown menu to match new clipNames and update database.
+        clipNames[selected] = input1Buffer.get_text();
+        clip[selected] = input2Buffer.get_text(input2Buffer.get_start_iter(), input2Buffer.get_end_iter(), false);
+        settings.set_strv('clip', clip);
+        settings.set_strv('clipnames', clipNames);
 
-		grid.remove(dropDown);
-		dropDown = Gtk.DropDown.new_from_strings(Array.from({ length: 10 }, (v, i) => clipNames[i] ));
-		grid.attach(dropDown, 4, 1, 2, 1);
-		dropDown.set_selected(selected);
-	});
-	grid.attach(button, 4, 10, 2, 1);
+        grid.remove(dropDown);
+        dropDown = Gtk.DropDown.new_from_strings(Array.from({ length: 10 }, (v, i) => clipNames[i] ));
+        grid.attach(dropDown, 4, 1, 2, 1);
+        dropDown.set_selected(selected);
+    });
+    grid.attach(button, 4, 10, 2, 1);
 
-	interval = setInterval(() => update(dropDown, input1Buffer, input2Buffer, grid, clip, clipNames, interval), 100);
+    interval = setInterval(() => update(dropDown, input1Buffer, input2Buffer, grid, clip, clipNames, interval), 100);
 
     window.connect('close-request', 
-	() => {
-		clearInterval(interval);
-	});
+    () => {
+        clearInterval(interval);
+    });
 
-	group.add(grid);
-	page.add(group);
-	window.add(page);
+    group.add(grid);
+    page.add(group);
+    window.add(page);
 }
